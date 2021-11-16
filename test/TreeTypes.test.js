076eb79780dcf07861ff3483399b02934b282d50
hnamzian <namazian87@gmail.com>
2021-11-16 12:25:47 +0300

    Test removing tree type by owner

--- test/TreeTypes.test.js
+++ test/TreeTypes.test.js
@@ -1,5 +1,6 @@
-const { expect } = require("chai");
-const { formatUnits, parseUnits, parseEther } = require("@ethersproject/units");
+const { solidity } = require("ethereum-waffle");
+const { expect } = require("chai").use(solidity);
+const { parseEther } = require("@ethersproject/units");
 
 let treeTypes;
 
@@ -22,5 +23,19 @@ describe("TreeTypes", async () => {
     expect(storedTreeType.name).to.be.eq(treeType.name);
     expect(storedTreeType.O2Rate).to.be.eq(treeType.O2Rate);
     expect(storedTreeType.price).to.be.eq(treeType.price);
+  });
+
+  it("should remove existing tree type by owner", async () => {
+    const treeType = {
+      name: "Cinar",
+      O2Rate: 100,
+      price: parseEther("0.001"),
+    };
+    await treeTypes.addTreeType(treeType.name, treeType.O2Rate, treeType.price);
+    await treeTypes.removeTreeTypeByName(treeType.name);
+
+    expect(treeTypes.getTreeTypeByName(treeType.name)).revertedWith(
+      "tree type name does not exist"
+    );
   });
 });
