9fbcb67ae64aeb2725b142331df5bab554986a34
hnamzian <namazian87@gmail.com>
2021-11-16 13:32:23 +0300

    Add getTreeById to return tree of a specified Id

--- contracts/Trees.sol
+++ contracts/Trees.sol
@@ -25,4 +25,34 @@ contract Trees {
     Tree[] internal _trees;
     // list of trees owned by each address
     mapping(address => EnumerableSet.UintSet) internal _treesOf;
+
+    /**
+     * @dev returns parameters of a tree with a specified Id
+     * @return typeName tree type name
+     * @return region region tree has been planted
+     * @return birthDate birth date of tree
+     * @return height height of tree
+     * @return diameter diameter of tree
+     * @return transferredAt timestamp tree ownership transfered
+     */
+    function getTreeById(uint256 treeId_)
+        public
+        view
+        returns (
+            string memory typeName,
+            string memory region,
+            uint256 birthDate,
+            uint8 height,
+            uint8 diameter,
+            uint256 transferredAt
+        )
+    {
+        Tree memory _tree = _trees[treeId_];
+        typeName = _tree.typeName;
+        region = _tree.region;
+        birthDate = _tree.birthDate;
+        height = _tree.height;
+        diameter = _tree.diameter;
+        transferredAt = _tree.transferredAt;
+    }
 }
