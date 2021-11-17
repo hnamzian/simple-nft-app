39e9201e56f57da98cd5b6162309cfffdc84dab6
hnamzian <namazian87@gmail.com>
2021-11-17 03:47:35 +0300

    Test oxygensOf after transferring tree

--- test/Oxygens.test.js
+++ test/Oxygens.test.js
@@ -1,7 +1,11 @@
 const { solidity } = require("ethereum-waffle");
 const { expect } = require("chai").use(solidity);
 const { parseEther } = require("@ethersproject/units");
-const { getTimestamp, increaseEVMTimestamp, increaseEVMTimestampAndMine } = require("./helper/evm");
+const {
+  getTimestamp,
+  increaseEVMTimestamp,
+  increaseEVMTimestampAndMine,
+} = require("./helper/evm");
 
 let treeToken;
 const treeType = {
@@ -20,8 +24,6 @@ describe("Oxygens", async () => {
 
   it("should update oxygen balance of tree holder", async () => {
     const [owner, alice] = await ethers.getSigners();
-
-    const aliceInitBalance = await alice.getBalance();
 
     const tree = {
       typeName: treeType.name,
@@ -50,5 +52,58 @@ describe("Oxygens", async () => {
     const oxygens = await treeToken.oxygensOf(alice.address);
     const desiredO2 = waitTime * treeType.O2Rate;
     expect(oxygens).to.be.eq(desiredO2);
+  });
+
+  it("should update oxygen balance of tree holder after transfering tree", async () => {
+    const [owner, alice, bob] = await ethers.getSigners();
+
+    const tree = {
+      typeName: treeType.name,
+      region: "Brazil",
+      birthDate: Date.now(),
+      height: 1,
+      diameter: 10,
+    };
+
+    await treeToken
+      .connect(alice)
+      .purchaseTree(
+        tree.typeName,
+        tree.region,
+        tree.birthDate,
+        tree.height,
+        tree.diameter,
+        {
+          value: treeType.price,
+        }
+      );
+
+    const whenAlicePurchasedTree = await getTimestamp();
+
+    const waitTime = 100;
+    await increaseEVMTimestampAndMine(waitTime);
+
+    const treeIds = await treeToken.getTreesOf(alice.address);
+    await treeToken.connect(alice).approve(bob.address, treeIds[0]);
+    await treeToken
+      .connect(bob)
+      .transferFrom(alice.address, bob.address, treeIds[0]);
+
+    const whenAliceTransferredTreeToBob = await getTimestamp();
+    const aliceDesiredO2 =
+      (whenAliceTransferredTreeToBob - whenAlicePurchasedTree) *
+      treeType.O2Rate;
+
+    await increaseEVMTimestampAndMine(waitTime);
+
+    const whenBobClaimedOxygens = await getTimestamp();
+
+    const bobDesiredO2 =
+      (whenBobClaimedOxygens - whenAliceTransferredTreeToBob) * treeType.O2Rate;
+
+    const aliceO2 = await treeToken.oxygensOf(alice.address);
+    const bobO2 = await treeToken.oxygensOf(bob.address);
+    expect(aliceO2).to.be.eq(aliceDesiredO2);
+    expect(bobO2).to.be.eq(bobDesiredO2);
   });
 });
